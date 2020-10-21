52c8f86733129118c932083196cf71921873f2e4
Emrikcah <curryandr3@gmail.com>
2020-10-21 09:49:13 -0400

    added javascript

---
+++ dist/js/app.js
@@ -0,0 +1,22 @@
+const form = document.querySelector(".input-wrapper");
+const input = document.querySelector(".input");
+
+
+function checkEmail(e) {
+    e.preventDefault();
+  const inputValue = input.value;
+  if (validateEmail(inputValue) !== true) {
+     form.classList.add('error')
+  } else {
+   form.classList.remove('error')
+  }
+ 
+}
+
+// validateEmail() taken from stackoverflow https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+function validateEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+form.addEventListener("submit", checkEmail);
